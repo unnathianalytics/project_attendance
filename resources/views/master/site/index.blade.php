5b54438bd2a73b577fc265ed3a020ff098078d0d
unnathianalytics <unnathianalytics@gmail.com>
2025-09-02 08:15:40 +0530

    update 7 files

--- resources/views/master/site/index.blade.php
+++ resources/views/master/site/index.blade.php
@@ -8,7 +8,7 @@
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-end">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a wire:navigate href="{{ route('dashboard') }}">Home</a></li>
                         <li class="breadcrumb-item active" aria-current="page">
                             Site List
                         </li>
