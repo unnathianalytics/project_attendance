8895c20c5d0599f281ecae30eaa7f3ffe030ff9e
Unnathi Analytics <unnathianalytics@gmail.com>
2025-09-03 13:29:27 +0530

    update js.blade.php and style.blade.php

--- resources/views/layouts/js.blade.php
+++ resources/views/layouts/js.blade.php
@@ -2,6 +2,7 @@
 <script src="{{ asset('dist/js/jquery-3.7.1.min.js') }}"></script>
 <script src="{{ asset('dist/js/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ asset('dist/js/adminlte.js') }}"></script>
+@livewireScripts
 <script>
     const SELECTOR_SIDEBAR_WRAPPER = ".sidebar-wrapper"
     const Default = {
--- resources/views/layouts/style.blade.php
+++ resources/views/layouts/style.blade.php
@@ -2,3 +2,4 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css"
     crossorigin="anonymous">
 <link rel="stylesheet" href="{{ asset('dist/css/custom.css') }}">
+@livewireStyles
