24aaa0757c32be8c449881482f6b7450db0abf79
Unnathi Analytics <unnathianalytics@gmail.com>
2025-09-03 16:10:49 +0530

    update custom.css and style.blade.php

--- resources/views/layouts/style.blade.php
+++ resources/views/layouts/style.blade.php
@@ -1,5 +1,7 @@
+<link rel="preload" href="{{ asset('dist/css/adminlte.css') }}?v{{ rand(1000, 99999999) }}" as="style">
 <link rel="preload" href="{{ asset('dist/css/custom.css') }}?v{{ rand(1000, 99999999) }}" as="style">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css"
     crossorigin="anonymous">
+<link rel="stylesheet" href="{{ asset('dist/css/adminlte.css') }}?v{{ rand(1000, 99999999) }}">
 <link rel="stylesheet" href="{{ asset('dist/css/custom.css') }}?v{{ rand(1000, 99999999) }}">
 @livewireStyles
