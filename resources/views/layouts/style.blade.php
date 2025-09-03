5de3b95fb1de36cc8b9214798ef0f885f8a20071
Unnathi Analytics <unnathianalytics@gmail.com>
2025-09-03 15:58:59 +0530

    update style.blade.php

--- resources/views/layouts/style.blade.php
+++ resources/views/layouts/style.blade.php
@@ -1,5 +1,5 @@
-<link rel="preload" href="{{ asset('dist/css/custom.css') }}" as="style">
+<link rel="preload" href="{{ asset('dist/css/custom.css') }}?{{ app()->version() }}" as="style">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css"
     crossorigin="anonymous">
-<link rel="stylesheet" href="{{ asset('dist/css/custom.css') }}">
+<link rel="stylesheet" href="{{ asset('dist/css/custom.css') }}?{{ app()->version() }}">
 @livewireStyles
