b4cb2224f5f9e2b055b542dab11de9e31d293237
unnathianalytics <unnathianalytics@gmail.com>
2025-10-22 10:06:41 +0530

    feat: Implement new sidebar navigation
    - Added Masters & Transctions groups
    - Labor, Customers, Sites menu items
    - Links to create and list pages

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,134 +1,3 @@
-{{-- old --}}
-{{-- <nav class="app-header navbar navbar-expand bg-primary-subtle" data-bs-theme="dark">
-    <div class="container-fluid">
-        <ul class="navbar-nav">
-            <li class="nav-item"> <a class="nav-link" data-lte-toggle="sidebar" href="#" role="button">
-                    <i class="bi bi-list"></i> </a> </li>
-            <li class="nav-item d-none d-md-block"> <a href="#" class="nav-link">Home</a> </li>
-            <li class="nav-item d-none d-md-block"> <a href="#" class="nav-link">Contact</a> </li>
-        </ul>
-        <ul class="navbar-nav ms-auto">
-            <li class="nav-item"> <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-                    <i class="bi bi-search"></i> </a> </li>
-            <li class="nav-item dropdown"> <a class="nav-link" data-bs-toggle="dropdown" href="#"> <i
-                        class="bi bi-chat-text"></i> <span class="navbar-badge badge text-bg-danger">3</span>
-                </a>
-                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end"> <a href="#" class="dropdown-item">
-                        <div class="d-flex">
-                            <div class="shrink-0"> <img src="{{ asset('dist/assets/img/user1-128x128.jpg') }}"
-                                    alt="User Avatar" class="img-size-50 rounded-circle me-3"> </div>
-                            <div class="flex-grow-1">
-                                <h3 class="dropdown-item-title">
-                                    Brad Diesel
-                                    <span class="float-end fs-7 text-danger"><i class="bi bi-star-fill"></i></span>
-                                </h3>
-                                <p class="fs-7">Call me whenever you can...</p>
-                                <p class="fs-7 text-secondary"> <i class="bi bi-clock-fill me-1"></i> 4 Hours
-                                    Ago
-                                </p>
-                            </div>
-                        </div>
-                    </a>
-                    <div class="dropdown-divider"></div> <a href="#" class="dropdown-item">
-                        <div class="d-flex">
-                            <div class="shrink-0"> <img src="{{ asset('dist/assets/img/user8-128x128.jpg') }}"
-                                    alt="User Avatar" class="img-size-50 rounded-circle me-3"> </div>
-                            <div class="flex-grow-1">
-                                <h3 class="dropdown-item-title">
-                                    John Pierce
-                                    <span class="float-end fs-7 text-secondary"> <i class="bi bi-star-fill"></i>
-                                    </span>
-                                </h3>
-                                <p class="fs-7">I got your message bro</p>
-                                <p class="fs-7 text-secondary"> <i class="bi bi-clock-fill me-1"></i> 4 Hours
-                                    Ago
-                                </p>
-                            </div>
-                        </div>
-                    </a>
-                    <div class="dropdown-divider"></div> <a href="#" class="dropdown-item">
-                        <div class="d-flex">
-                            <div class="shrink-0"> <img src="{{ asset('dist/assets/img/user3-128x128.jpg') }}"
-                                    alt="User Avatar" class="img-size-50 rounded-circle me-3"> </div>
-                            <div class="flex-grow-1">
-                                <h3 class="dropdown-item-title">
-                                    Nora Silvester
-                                    <span class="float-end fs-7 text-warning"> <i class="bi bi-star-fill"></i>
-                                    </span>
-                                </h3>
-                                <p class="fs-7">The subject goes here</p>
-                                <p class="fs-7 text-secondary"> <i class="bi bi-clock-fill me-1"></i> 4 Hours
-                                    Ago
-                                </p>
-                            </div>
-                        </div>
-                    </a>
-                    <div class="dropdown-divider"></div> <a href="#" class="dropdown-item dropdown-footer">See All
-                        Messages</a>
-                </div>
-            </li>
-            <li class="nav-item dropdown"> <a class="nav-link" data-bs-toggle="dropdown" href="#"> <i
-                        class="bi bi-bell-fill"></i> <span class="navbar-badge badge text-bg-warning">15</span> </a>
-                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end"> <span
-                        class="dropdown-item dropdown-header">15 Notifications</span>
-                    <div class="dropdown-divider"></div> <a href="#" class="dropdown-item"> <i
-                            class="bi bi-envelope me-2"></i> 4 new messages
-                        <span class="float-end text-secondary fs-7">3 mins</span> </a>
-                    <div class="dropdown-divider"></div> <a href="#" class="dropdown-item"> <i
-                            class="bi bi-people-fill me-2"></i> 8 friend requests
-                        <span class="float-end text-secondary fs-7">12 hours</span> </a>
-                    <div class="dropdown-divider"></div> <a href="#" class="dropdown-item"> <i
-                            class="bi bi-file-earmark-fill me-2"></i> 3 new reports
-                        <span class="float-end text-secondary fs-7">2 days</span> </a>
-                    <div class="dropdown-divider"></div> <a href="#" class="dropdown-item dropdown-footer">
-                        See All Notifications
-                    </a>
-                </div>
-            </li>
-            <li class="nav-item"> <a class="nav-link" href="#" data-lte-toggle="fullscreen"> <i
-                        data-lte-icon="maximize" class="bi bi-arrows-fullscreen"></i> <i data-lte-icon="minimize"
-                        class="bi bi-fullscreen-exit" style="display: none;"></i>
-                </a> </li>
-            <li class="nav-item dropdown user-menu"> <a href="#" class="nav-link dropdown-toggle"
-                    data-bs-toggle="dropdown"> <img src="{{ asset('dist/assets/img/user2-160x160.jpg') }}"
-                        class="user-image rounded-circle shadow-sm" alt="User Image"> <span
-                        class="d-none d-md-inline">{{ auth()->user()->name }}</span> </a>
-                <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
-                    <li class="user-header text-bg-primary"> <img
-                            src="{{ asset('dist/assets/img/user2-160x160.jpg') }}" class="rounded-circle shadow-sm"
-                            alt="User Image">
-                        <p>
-                            {{ auth()->user()->name }} - <span class="text-xs">
-                                @foreach (auth()->user()->getRoleNames() as $role)
-                                    {{ Str::headline($role) }}
-                                @endforeach
-                            </span>
-                            <small>{{ auth()->user()->created_at->format('M. Y') }}</small>
-                        </p>
-                    </li>
-                    <li class="user-body">
-                        <div class="row">
-                            <div class="col-4 text-center"> <a href="#">Followers</a> </div>
-                            <div class="col-4 text-center"> <a href="#">Sales</a> </div>
-                            <div class="col-4 text-center"> <a href="#">Friends</a> </div>
-                        </div>
-                    </li>
-                    <li class="user-footer"> <a href="#" class="btn btn-default btn-flat">Profile</a>
-                        <form id="logout-form" method="POST" action="{{ route('logout') }}" style="display: none;">
-                            @csrf
-                        </form>
-                        <a href="{{ route('logout') }}" class="btn btn-danger btn-flat float-end"
-                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                            Logout
-                        </a>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-    </div>
-</nav>
-<x-aside /> --}}
-{{-- old --}}
 <flux:sidebar sticky collapsible="mobile"
     class="bg-zinc-50 dark:bg-zinc-900 border-r border-zinc-200 dark:border-zinc-700">
     <flux:sidebar.header>
@@ -138,19 +7,37 @@
     </flux:sidebar.header>
     {{-- <flux:sidebar.search placeholder="Search..." /> --}}
     <flux:sidebar.nav>
-        <flux:sidebar.item icon="home" href="#" current>Home</flux:sidebar.item>
+        {{-- <flux:sidebar.item icon="home" href="#" current>Home</flux:sidebar.item>
         <flux:sidebar.item icon="inbox" badge="12" href="#">Inbox</flux:sidebar.item>
         <flux:sidebar.item icon="document-text" href="#">Documents</flux:sidebar.item>
-        <flux:sidebar.item icon="calendar" href="#">Calendar</flux:sidebar.item>
-        <flux:sidebar.group expandable heading="Favorites" class="grid">
-            <flux:sidebar.item href="#">Marketing site</flux:sidebar.item>
-            <flux:sidebar.item href="#">Android app</flux:sidebar.item>
-            <flux:sidebar.group expandable heading="Sub" class="grid">
-                <flux:sidebar.item href="#">Marketing site</flux:sidebar.item>
-                <flux:sidebar.item href="#">Android app</flux:sidebar.item>
-                <flux:sidebar.item href="#">Brand guidelines</flux:sidebar.item>
+        <flux:sidebar.item icon="calendar" href="#">Calendar</flux:sidebar.item> --}}
+        <flux:sidebar.group expandable expanded="false" heading="Masters" class="grid">
+            <flux:sidebar.group expandable expanded="false" badge="{{ \App\Models\Labor::count() }}" heading="Labor" class="grid">
+                <flux:sidebar.item href="{{ route('labor.create') }}">Create</flux:sidebar.item>
+                <flux:sidebar.item href="{{ route('labor.index') }}">List</flux:sidebar.item>
             </flux:sidebar.group>
-            <flux:sidebar.item href="#">Brand guidelines</flux:sidebar.item>
+             <flux:sidebar.group expandable expanded="false" badge="{{ \App\Models\Customer::count() }}" heading="Customers" class="grid">
+                <flux:sidebar.item href="{{ route('customer.create') }}">Create</flux:sidebar.item>
+                <flux:sidebar.item href="{{ route('customer.index') }}">List</flux:sidebar.item>
+            </flux:sidebar.group>
+            <flux:sidebar.group expandable expanded="false" badge="{{ \App\Models\Site::count() }}" heading="Sites" class="grid">
+                <flux:sidebar.item href="{{ route('site.create') }}">Create</flux:sidebar.item>
+                <flux:sidebar.item href="{{ route('site.index') }}">List</flux:sidebar.item>
+            </flux:sidebar.group>
+        </flux:sidebar.group>
+        <flux:sidebar.group expandable heading="Transactions" class="grid">
+            <flux:sidebar.group expandable expanded="false" badge="{{ \App\Models\Labor::count() }}" heading="Labor" class="grid">
+                <flux:sidebar.item href="{{ route('labor.create') }}">Create</flux:sidebar.item>
+                <flux:sidebar.item href="{{ route('labor.index') }}">List</flux:sidebar.item>
+            </flux:sidebar.group>
+             <flux:sidebar.group expandable expanded="false" badge="{{ \App\Models\Customer::count() }}" heading="Customers" class="grid">
+                <flux:sidebar.item href="{{ route('customer.create') }}">Create</flux:sidebar.item>
+                <flux:sidebar.item href="{{ route('customer.index') }}">List</flux:sidebar.item>
+            </flux:sidebar.group>
+            <flux:sidebar.group expandable expanded="false" badge="{{ \App\Models\Site::count() }}" heading="Sites" class="grid">
+                <flux:sidebar.item href="{{ route('site.create') }}">Create</flux:sidebar.item>
+                <flux:sidebar.item href="{{ route('site.index') }}">List</flux:sidebar.item>
+            </flux:sidebar.group>
         </flux:sidebar.group>
     </flux:sidebar.nav>
     <flux:sidebar.spacer />
